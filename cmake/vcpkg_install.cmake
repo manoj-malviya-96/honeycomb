f48c88aee3f932f898efb9cafd9560530353008b
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-08-23 13:44:38 +0900

    add vcpkg through submodules approach

---
+++ cmake/vcpkg_install.cmake
@@ -0,0 +1,20 @@
+set(VCPKG_ROOT "${CMAKE_SOURCE_DIR}/thirdparty/vcpkg")
+set(VCPKG_BINARY "${VCPKG_ROOT}/vcpkg")
+
+# Bootstrap vcpkg if necessary
+if (NOT EXISTS "${VCPKG_BINARY}")
+    message(STATUS "Bootstrapping vcpkg in ${VCPKG_ROOT}…")
+    execute_process(
+            COMMAND "${VCPKG_ROOT}/bootstrap-vcpkg.sh"
+            WORKING_DIRECTORY "${VCPKG_ROOT}"
+            RESULT_VARIABLE _vcpkg_bootstrap_res
+            OUTPUT_VARIABLE _vcpkg_bootstrap_out
+            ERROR_VARIABLE _vcpkg_bootstrap_err
+    )
+    if (NOT _vcpkg_bootstrap_res EQUAL 0)
+        message(FATAL_ERROR
+                "vcpkg bootstrap failed:\n${_vcpkg_bootstrap_err}\n${_vcpkg_bootstrap_out}")
+    endif ()
+endif ()
+
+set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/thirdparty/vcpkg/scripts/buildsystems/vcpkg.cmake")
