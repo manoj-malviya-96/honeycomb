562132d6664c8eb3c640d54c32d41816a9dd54bf
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-08-25 20:07:26 +0700

    simply add cxxopts to have optional inputs

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,31 @@
 //
 
 #include <iostream>
+#include "cxxopts.hpp"
 #include "honeycomb.h"
 #include "vtk_renderer.h"
 
+int main(int argc, char* argv[]) {
 
-int main() {
-    constexpr std::size_t numRows = 20;
-    constexpr std::size_t numCols = 20;
-    constexpr double cellSize = 1.0;
+    cxxopts::Options options(argv[0], "Honeycomb Frame Generator");
+    options.add_options()
+        ("r,numRows", "Number of rows in the grid", cxxopts::value<std::size_t>()->default_value("20"))
+        ("c,numCols", "Number of columns in the grid", cxxopts::value<std::size_t>()->default_value("20"))
+        ("s,edgeLength", "The edge length of a single hexagon", cxxopts::value<double>()->default_value("1.0"))
+        ("h,help", "Print usage information");
 
-    const auto skeleton = HoneyComb::Frame::generateHoneyCombSkeleton({numRows, numCols, cellSize});
+    const auto result = options.parse(argc, argv);
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    const auto numRows = result["numRows"].as<std::size_t>();
+    const auto numCols = result["numCols"].as<std::size_t>();
+    const auto edgeLength = result["edgeLength"].as<double>();
+    const auto skeleton = HoneyComb::Frame::generateHoneyCombSkeleton({numRows, numCols, edgeLength});
+
     HoneyComb::Renderer::renderSkeleton(skeleton);
+
     return EXIT_SUCCESS;
 }
