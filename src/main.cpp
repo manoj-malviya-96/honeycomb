a461bbdceaa7331145769b6811b9f1d570f4f0f6
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-08-25 19:43:05 +0700

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,17 @@
 // Created by Manoj Malviya on 8/23/25.
 //
 
+#include <iostream>
+#include "honeycomb.h"
+#include "vtk_renderer.h"
+
 
 int main() {
-    return 0;
+    constexpr std::size_t numRows = 20;
+    constexpr std::size_t numCols = 20;
+    constexpr double cellSize = 1.0;
+
+    const auto skeleton = HoneyComb::Frame::generateHoneyCombSkeleton({numRows, numCols, cellSize});
+    HoneyComb::Renderer::renderSkeleton(skeleton);
+    return EXIT_SUCCESS;
 }
